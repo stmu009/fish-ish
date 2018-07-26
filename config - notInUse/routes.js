a73c8c5716dfccd70c900ff67e59accc297e0616
Andrea Velardi <>
2018-07-26 18:57:14 -0500

    updates to folder structure - still not connecting to mongoor local host

---
+++ config - notInUse/routes.js
@@ -0,0 +1,11 @@
+// module.exports = function(router) {
+//     // This route renders the homepage
+//     router.get("/", function (req, res) {
+//         res.render("home");
+//     });
+// }
+
+
+
+
+
--- config/routes.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(router) {
-    // This route renders the homepage
-    router.get("/", function (req, res) {
-        res.render("home");
-    });
-}
-
-
-
