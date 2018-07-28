b4127d94fb3de1a86baedab3f1a8534c1df39c2d
Tracer16 <tracermagee@gmail.com>
2018-07-28 13:41:41 -0500

    did some more passport stuff... Dre is finishing this up

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,6 +1,14 @@
     const router = require("express").Router();
     const userController = require("../../controllers/userController");
 
+    // app.post('/signup', passport.authenticate('local-signup',  { successRedirect: '/dashboard',
+    //                                                 failureRedirect: '/signup'}
+    //                                                 ));
+
+    // app.post('/signin', passport.authenticate('local-signin',  { successRedirect: '/dashboard',
+    //                                                 failureRedirect: '/signin'}
+    //                                                 ));
+
     // Matches with "/api/user"
     router.route("/")
     .get(userController.findAll)
