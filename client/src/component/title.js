386f11592db441bfdd68dfd66101aeebb0c1ec3b
Tracer16 <tracermagee@gmail.com>
2018-08-02 18:52:44 -0500

    worked on login

---
+++ client/src/component/title.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Title extends Component {
+  render() {
+    return (
+      <div className="Title">"Fish-ish"</div>
+    );
+  }
+}
+
+export default Title;
