953de3eeae8edaeeedf1dd11d8c0caa5acb67b2e
Tracer16 <tracermagee@gmail.com>
2018-08-09 17:01:16 -0500

    updated the game banner

--- client/src/components/banner.js
+++ client/src/components/banner.js
@@ -5,6 +5,9 @@ var bannerStyle = {
     position: "fixed",
     bottom: 0,
     backgroundColor: "gold",
+    border: "orange 3px solid",
+    fontFamily: "luckiest guy",
+    fontSize: "35px",
     width: "100%",
     height: "4rem",
     padding: "1rem",
@@ -15,8 +18,8 @@ class Game extends Component {
     render() {
         return (
             <div style={bannerStyle}>
-                <span>Level: 1  </span>
-                <span>Lives: 3  </span>
+                <span>Level: 1  </span><span> | </span>
+                <span>Lives: 3  </span><span> | </span>
                 <span>Score: 0  </span>
                 <span style={{ float: "right" }}>Use the arrows to move</span>
             </div>
